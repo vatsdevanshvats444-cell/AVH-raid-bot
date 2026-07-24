fdbeb0da646c4a337a9377be1bd9033a08e1e7da
vatsdevanshvats444-cell <vatsdevanshvats444@gmail.com>
2026-07-24 15:57:29 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`✅ ${client.user.tag} is online!`);
+});
+
+client.login(process.env.TOKEN);
